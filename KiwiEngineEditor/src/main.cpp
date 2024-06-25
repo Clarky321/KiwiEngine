8cbbd52e17f637a4cfe858f19b8b56cb0c0bc250
LORAN <vinana222@gmail.com>
2024-06-25 23:21:15 +0300

    test project

--- KiwiEngineEditor/src/main.cpp
+++ KiwiEngineEditor/src/main.cpp
@@ -1,14 +1,40 @@
-#include <KiwiEngineCore\Application.h>
-
-#include <iostream>
+#include <raylib.h>
 
 int main()
 {
-	std::cout << "Hello KiwiEngineEditor project" << std::endl;
+	// Window
+	int width = 800;
+	int height = 600;
 
-	std::cout << " " << std::endl;
+	InitWindow(width, height, "Window");
 
-	Application();
+	// Cirlce
+	int circle_x = 400;
+	int circle_y = 300;
+	int radius = 50;
+
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+
+		// Game login begins
+
+		DrawCircle(circle_x, circle_y, radius, SKYBLUE);
+
+		if (IsKeyDown(KEY_D) && circle_x < 800) { circle_x = circle_x + 5; }
+
+		if (IsKeyDown(KEY_A) && circle_x > 0) { circle_x = circle_x - 5; }
+
+		if (IsKeyDown(KEY_W) && circle_y > 0) { circle_y = circle_y - 5; }
+
+		if (IsKeyDown(KEY_S) && circle_y < 600) { circle_y = circle_y + 5; }
+
+		// Game login ends
+		EndDrawing();
+	}
 
 	return 0;
 }
