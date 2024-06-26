57b5e60606847edc1729c8c1f43e27e59d59e2ef
Рябов Сергей Дмитриевич <ryabov-sd@ranepa.ru>
2024-06-26 14:29:11 +0300

    circle and rectangle

--- KiwiEngineEditor/src/main.cpp
+++ KiwiEngineEditor/src/main.cpp
@@ -13,6 +13,12 @@ int main()
 	int circle_y = 300;
 	int radius = 50;
 
+	// Rectangle
+	int rectangle_x = 200;
+	int rectangle_y = 100;
+	int rectangle_width = 100;
+	int rectangle_height = 100;
+
 	SetTargetFPS(60);
 
 	while (!WindowShouldClose())
@@ -31,6 +37,16 @@ int main()
 		if (IsKeyDown(KEY_W) && circle_y > 0) { circle_y = circle_y - 5; }
 
 		if (IsKeyDown(KEY_S) && circle_y < 600) { circle_y = circle_y + 5; }
+
+		DrawRectangle(rectangle_x, rectangle_y, rectangle_width, rectangle_height, RED);
+
+		if (IsKeyDown(KEY_RIGHT) && rectangle_x < 700) { rectangle_x = rectangle_x + 5; }
+
+		if (IsKeyDown(KEY_LEFT) && rectangle_x > 0) { rectangle_x = rectangle_x - 5; }
+
+		if (IsKeyDown(KEY_UP) && rectangle_y > 0) { rectangle_y = rectangle_y - 5; }
+
+		if (IsKeyDown(KEY_DOWN) && rectangle_y < 500) { rectangle_y = rectangle_y + 5; }
 
 		// Game login ends
 		EndDrawing();
